f118e144b0822e78e92966d5fa5136e4c43e346a
Louispandamesiregar <louispandame@gmail.com>
2026-07-21 22:23:52 +0700

    perf: convert fleet images to WebP to fix slow initial load times
    - Replaced 500KB PNGs with 40KB WebPs (90% size reduction)
    - Fixes issue where CDE Box image loads slowly when first opened
    - Deleted old PNG assets

--- src/components/FleetSection.tsx
+++ src/components/FleetSection.tsx
@@ -4,10 +4,10 @@ import * as React from "react"
 import Image from "next/image"
 import { useLanguage } from "@/context/LanguageContext"
 import { Truck, Package, Maximize, ArrowRight, CheckCircle2 } from "lucide-react"
-import blindVanImg from "../../public/fleet/blind-van.png"
-import cdeBoxImg from "../../public/fleet/cde-box.png"
-import cddBoxImg from "../../public/fleet/cdd-box.png"
-import trontonWingboxImg from "../../public/fleet/tronton-wingbox.png"
+import blindVanImg from "../../public/fleet/blind-van.webp"
+import cdeBoxImg from "../../public/fleet/cde-box.webp"
+import cddBoxImg from "../../public/fleet/cdd-box.webp"
+import trontonWingboxImg from "../../public/fleet/tronton-wingbox.webp"
 
 const FLEET_DATA = [
   {
